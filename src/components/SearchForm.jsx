e1d0356875f9b6445bb63fddc53a6d82be3b8ae7
William Mathewson <wncmathewson@me.com>
2018-07-12 13:06:24 +0100

    Add SearchForm component

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Button, Icon } from 'react-materialize';
+import PropTypes from 'prop-types';
+
+//import axios from '../axios';
+
+class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    this.props.search(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="input-field inline">
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="e.g. Back to the Future"
+            required />
+        </div>
+        <Button waves="light" type="submit"><Icon>search</Icon></Button>
+      </form>
+    );
+  }
+}
+
+SearchForm.propTypes = {
+  search: PropTypes.func,
+}
+
+export default SearchForm;
